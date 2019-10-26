b93cd763eaa7f069d45c7935d8da754c22c7ccd4
Richard Skumat <gitlab@richardskumat.com>
2019-10-26 17:49:05 +0100

    update local remote[skip ci]

--- add_remotes_back.sh
+++ add_remotes_back.sh
@@ -1,3 +1,3 @@
 git remote add github git@github:richardskumat/ansible-role-samba
 git remote add gitlab git@gitlab:richardskumat/ansible-role-samba
-git remote add origin git@wsgitlab:qwe/ansible-role-samba
+git remote add origin git@gitea:qwe/ansible-role-samba
